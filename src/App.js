661b9a5a48056b53fb172d7cc16aab8d5f873366
revyrob <kayle.robson@gmail.com>
2023-05-06 08:54:47 -0700

    updated browser router page'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.scss";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import ProjectList from "./components/ProjectList/ProjectList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AboutPage from "./pages/AboutPage";
+import ProjectPage from "./pages/ProjectPage";
+import NotFound from "./pages/NotFound";
 import ReactGA from "react-ga";
-import home from "./assets/icons/home-icon.svg";
-import info from "./assets/icons/info-icon.png";
-import project from "./assets/icons/project-icon.png";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { HashLink as Link } from "react-router-hash-link";
-import About from "./components/About/About";
-import ProjectData from "./data/projects.json";
-
+import { useEffect } from "react";
 const TRACKING_ID = "UA-212922012-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 function App() {
@@ -20,32 +15,15 @@ function App() {
   }, []);
   //local storage for light and dark theme
   //set light and dark them in app.js and pass it down to all pages
-  const location = useLocation(); // once ready it returns the 'window.location' object
-  const [url, setUrl] = useState(null);
-  useEffect(() => {
-    setUrl(location.pathname);
-  }, [location]);
-
-  //create a state with project data
-  const [projects] = useState(ProjectData);
   return (
-    <>
-      <BrowserRouter>
-        <header className="bottomNav">
-          <Link className="bottomNav__link" to="">
-            Home
-          </Link>
-          <Link className="bottomNav__link" to="">
-            About
-          </Link>
-          <Link className="bottomNav__link" to="#projects">
-            Projects
-          </Link>
-        </header>
-      </BrowserRouter>
-      <About />
-      <ProjectList projects={projects} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/About" element={<AboutPage />} />
+        <Route path="/Projects" element={<AboutPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
