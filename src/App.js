df0d84d0caed2964efae7f930d6a1739893cbcb0
revyrob <kayle.robson@gmail.com>
2023-05-06 00:31:14 -0700

    working to make it a one page application

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import AboutPage from "./pages/AboutPage";
 import ProjectPage from "./pages/ProjectPage";
 import NotFound from "./pages/NotFound";
 import ReactGA from "react-ga";
-import { useEffect } from "react";
+import home from "./assets/icons/home-icon.svg";
+import info from "./assets/icons/info-icon.png";
+import project from "./assets/icons/project-icon.png";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
+
 const TRACKING_ID = "UA-212922012-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 function App() {
@@ -15,14 +21,55 @@ function App() {
   }, []);
   //local storage for light and dark theme
   //set light and dark them in app.js and pass it down to all pages
+  const location = useLocation(); // once ready it returns the 'window.location' object
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/About" element={<AboutPage />} />
-        <Route path="/Projects" element={<AboutPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <section className="bottomNav">
+        <Link className="bottomNav__link" to="/">
+          <img
+            src={home}
+            className={
+              "bottomNav__logo filter-white" + (url === "/" ? " activeBot" : "")
+            }
+            alt="Brainflix logo"
+          />
+          <p className={"bottomNav__p " + (url === "/" ? " activep" : "")}>
+            Home
+          </p>
+        </Link>
+        <Link className="bottomNav__link" to="/about">
+          <img
+            src={info}
+            className={
+              "bottomNav__logo filter-white" +
+              (url === "/about" ? " activeBot" : "")
+            }
+            alt="Brainflix logo"
+          />
+          <p className={"bottomNav__p" + (url === "/about" ? " activep" : "")}>
+            About
+          </p>
+        </Link>
+        <Link className="bottomNav__link" to="/projects">
+          <img
+            src={project}
+            className={
+              "bottomNav__logo filter-white" +
+              (url === "/projects" ? " activeBot" : "")
+            }
+            alt="Brainflix logo"
+          />
+          <p
+            className={"bottomNav__p" + (url === "/projects" ? " activep" : "")}
+          >
+            Projects
+          </p>
+        </Link>
+      </section>
     </BrowserRouter>
   );
 }
