c79049b271fa91050061c769eec0234bd68b2601
revyrob <kayle.robson@gmail.com>
2023-05-06 00:54:57 -0700

    creating the app in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.scss";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import AboutPage from "./pages/AboutPage";
-import ProjectPage from "./pages/ProjectPage";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter } from "react-router-dom";
+import ProjectList from "./components/ProjectList/ProjectList";
 import ReactGA from "react-ga";
 import home from "./assets/icons/home-icon.svg";
 import info from "./assets/icons/info-icon.png";
@@ -12,6 +9,8 @@ import project from "./assets/icons/project-icon.png";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
+import About from "./components/About/About";
+import ProjectData from "../data/projects.json";
 
 const TRACKING_ID = "UA-212922012-1"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
@@ -26,6 +25,9 @@ function App() {
   useEffect(() => {
     setUrl(location.pathname);
   }, [location]);
+
+  //create a state with project data
+  const [projects] = useState(ProjectData);
   return (
     <BrowserRouter>
       <section className="bottomNav">
@@ -35,33 +37,33 @@ function App() {
             className={
               "bottomNav__logo filter-white" + (url === "/" ? " activeBot" : "")
             }
-            alt="Brainflix logo"
+            alt="home"
           />
           <p className={"bottomNav__p " + (url === "/" ? " activep" : "")}>
             Home
           </p>
         </Link>
-        <Link className="bottomNav__link" to="/about">
+        <Link className="bottomNav__link" to="#about">
           <img
             src={info}
             className={
               "bottomNav__logo filter-white" +
               (url === "/about" ? " activeBot" : "")
             }
-            alt="Brainflix logo"
+            alt="info"
           />
           <p className={"bottomNav__p" + (url === "/about" ? " activep" : "")}>
             About
           </p>
         </Link>
-        <Link className="bottomNav__link" to="/projects">
+        <Link className="bottomNav__link" to="#projects">
           <img
             src={project}
             className={
               "bottomNav__logo filter-white" +
               (url === "/projects" ? " activeBot" : "")
             }
-            alt="Brainflix logo"
+            alt="projects"
           />
           <p
             className={"bottomNav__p" + (url === "/projects" ? " activep" : "")}
@@ -70,6 +72,8 @@ function App() {
           </p>
         </Link>
       </section>
+      <About />
+      <ProjectList projects={projects} />
     </BrowserRouter>
   );
 }
